f35c47cabbc75b05286e335dc92206b3afb08825
Florian Forster <octo@leeloo.lan.home.verplant.org>
2010-05-10 10:38:55 +0200

    Update copyright date and email address.

--- src/sn_comparator.c
+++ src/sn_comparator.c
@@ -1,6 +1,6 @@
 /**
  * collectd - src/sn_comparator.c
- * Copyright (C) 2008  Florian octo Forster
+ * Copyright (C) 2008-2010  Florian octo Forster
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -16,7 +16,7 @@
  * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  *
  * Authors:
- *   Florian octo Forster <octo at verplant.org>
+ *   Florian octo Forster <ff at octo.it>
  **/
 
 #ifndef _ISOC99_SOURCE
